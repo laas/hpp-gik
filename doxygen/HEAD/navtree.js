776e9e72e2807e25c04b334571d090fc5909d653
Thomas Moulard <thomas.moulard@gmail.com>
2011-10-07 14:26:42 +0200

    Update HEAD Doxygen documentation. Source commit id: 4fa7d19e72ef6b15b7c9cac05a6761ff8c5a5862

---
+++ doxygen/HEAD/navtree.js
@@ -0,0 +1,447 @@
+var NAVTREE =
+[
+  [ "hpp-gik", "index.html", [
+    [ "GIK START POINT", "index.html", null ],
+    [ "Related Pages", "pages.html", [
+      [ "Example I", "a00002.html", null ]
+    ] ],
+    [ "Modules", "modules.html", [
+      [ "Abstract", "/home/thomas/profiles/laas/install/unstable/share/doc/jrl-mal/doxygen-html/a00032.html", null ],
+      [ "Boost", "/home/thomas/profiles/laas/install/unstable/share/doc/jrl-mal/doxygen-html/a00033.html", null ],
+      [ "Vector package", "/home/thomas/profiles/laas/install/unstable/share/doc/jrl-mal/doxygen-html/a00034.html", null ],
+      [ "Matrix package", "/home/thomas/profiles/laas/install/unstable/share/doc/jrl-mal/doxygen-html/a00035.html", null ],
+      [ "State Constraints", "a00107.html", null ],
+      [ "Prioritized State Constraints Solvers", "a00108.html", null ],
+      [ "Prioritized Motions", "a00109.html", null ],
+      [ "Prioritized Motions Plan", "a00110.html", null ],
+      [ "Prioritized Motions Solver", "a00111.html", null ],
+      [ "Dedicated Tasks", "a00112.html", null ],
+      [ "Robot", "a00113.html", null ],
+      [ "Tools", "a00114.html", null ]
+    ] ],
+    [ "Class List", "annotated.html", [
+      [ "ChppGik2DShape", "a00003.html", null ],
+      [ "ChppGik2DVertex", "a00004.html", null ],
+      [ "ChppGikBody", "a00005.html", null ],
+      [ "ChppGikComConstraint", "a00006.html", null ],
+      [ "ChppGikComMotion", "a00007.html", null ],
+      [ "ChppGikConfigurationConstraint", "a00008.html", null ],
+      [ "ChppGikFootDisplaceElement", "a00009.html", null ],
+      [ "ChppGikFootprint", "a00010.html", null ],
+      [ "ChppGikGazeConstraint", "a00011.html", null ],
+      [ "ChppGikGenericTask", "a00012.html", null ],
+      [ "ChppGikHalfSittingTask", "a00013.html", null ],
+      [ "ChppGikHandTask", "a00014.html", null ],
+      [ "ChppGikInterpolatedElement", "a00015.html", null ],
+      [ "ChppGikJointStateConstraint", "a00016.html", null ],
+      [ "ChppGikLinkedVertex", "a00017.html", null ],
+      [ "ChppGikLocomotionData", "a00018.html", null ],
+      [ "ChppGikLocomotionElement", "a00019.html", null ],
+      [ "ChppGikLocomotionPlan", "a00020.html", null ],
+      [ "ChppGikMaskFactory", "a00021.html", null ],
+      [ "ChppGikMetaTask", "a00022.html", null ],
+      [ "ChppGikMotionConstraint", "a00023.html", null ],
+      [ "ChppGikMotionPlan", "a00024.html", null ],
+      [ "ChppGikMotionPlanColumn", "a00025.html", null ],
+      [ "ChppGikMotionPlanElement", "a00026.html", null ],
+      [ "ChppGikMotionPlanRow", "a00027.html", null ],
+      [ "ChppGikNoLocomotion", "a00028.html", null ],
+      [ "ChppGikParallelConstraint", "a00029.html", null ],
+      [ "ChppGikPlaneConstraint", "a00030.html", null ],
+      [ "ChppGikPointingConstraint", "a00031.html", null ],
+      [ "ChppGikPositionConstraint", "a00032.html", null ],
+      [ "ChppGikPreviewController", "a00033.html", null ],
+      [ "ChppGikPrioritizedMotion", "a00034.html", null ],
+      [ "ChppGikPrioritizedStateConstraint", "a00035.html", null ],
+      [ "ChppGikReachTask", "a00036.html", null ],
+      [ "ChppGikReadyElement", "a00037.html", null ],
+      [ "ChppGikRobotTask", "a00038.html", null ],
+      [ "ChppGikRotationConstraint", "a00039.html", null ],
+      [ "ChppGikSolver", "a00040.html", null ],
+      [ "ChppGikSolverBasic", "a00041.html", null ],
+      [ "ChppGikSolverLNE", "a00042.html", null ],
+      [ "ChppGikStandingRobot", "a00043.html", null ],
+      [ "ChppGikStepBackTask", "a00044.html", null ],
+      [ "ChppGikStepElement", "a00045.html", null ],
+      [ "ChppGikStepTarget", "a00046.html", null ],
+      [ "ChppGikStepTask", "a00047.html", null ],
+      [ "ChppGikSupportPolygon", "a00048.html", null ],
+      [ "ChppGikTools", "a00049.html", null ],
+      [ "ChppGikTransformationConstraint", "a00050.html", null ],
+      [ "ChppGikVectorizableConstraint", "a00051.html", null ],
+      [ "ChppGikWalkElement", "a00052.html", null ],
+      [ "ChppGikWholeBodyTask", "a00053.html", null ],
+      [ "ChppGikZMPshiftElement", "a00054.html", null ],
+      [ "ChppRobotMotion", "a00055.html", null ],
+      [ "ChppRobotMotionSample", "a00056.html", null ]
+    ] ],
+    [ "Class Index", "classes.html", null ],
+    [ "Class Hierarchy", "hierarchy.html", [
+      [ "ChppGik2DShape", "a00003.html", null ],
+      [ "ChppGik2DVertex", "a00004.html", null ],
+      [ "ChppGikBody", "a00005.html", null ],
+      [ "ChppGikComConstraint", "a00006.html", null ],
+      [ "ChppGikFootprint", "a00010.html", null ],
+      [ "ChppGikJointStateConstraint", "a00016.html", [
+        [ "ChppGikParallelConstraint", "a00029.html", null ],
+        [ "ChppGikPlaneConstraint", "a00030.html", null ],
+        [ "ChppGikPointingConstraint", "a00031.html", [
+          [ "ChppGikGazeConstraint", "a00011.html", null ]
+        ] ],
+        [ "ChppGikPositionConstraint", "a00032.html", null ],
+        [ "ChppGikRotationConstraint", "a00039.html", null ],
+        [ "ChppGikTransformationConstraint", "a00050.html", null ]
+      ] ],
+      [ "ChppGikLinkedVertex", "a00017.html", null ],
+      [ "ChppGikLocomotionData", "a00018.html", null ],
+      [ "ChppGikLocomotionPlan", "a00020.html", null ],
+      [ "ChppGikMaskFactory", "a00021.html", null ],
+      [ "ChppGikMotionConstraint", "a00023.html", null ],
+      [ "ChppGikMotionPlan", "a00024.html", null ],
+      [ "ChppGikMotionPlanColumn", "a00025.html", null ],
+      [ "ChppGikMotionPlanElement", "a00026.html", null ],
+      [ "ChppGikMotionPlanRow", "a00027.html", null ],
+      [ "ChppGikPreviewController", "a00033.html", null ],
+      [ "ChppGikPrioritizedMotion", "a00034.html", [
+        [ "ChppGikComMotion", "a00007.html", null ],
+        [ "ChppGikInterpolatedElement", "a00015.html", null ],
+        [ "ChppGikLocomotionElement", "a00019.html", [
+          [ "ChppGikFootDisplaceElement", "a00009.html", null ],
+          [ "ChppGikStepElement", "a00045.html", null ],
+          [ "ChppGikWalkElement", "a00052.html", null ],
+          [ "ChppGikZMPshiftElement", "a00054.html", null ]
+        ] ],
+        [ "ChppGikNoLocomotion", "a00028.html", null ],
+        [ "ChppGikReadyElement", "a00037.html", null ]
+      ] ],
+      [ "ChppGikPrioritizedStateConstraint", "a00035.html", null ],
+      [ "ChppGikRobotTask", "a00038.html", [
+        [ "ChppGikGenericTask", "a00012.html", null ],
+        [ "ChppGikHalfSittingTask", "a00013.html", null ],
+        [ "ChppGikHandTask", "a00014.html", null ],
+        [ "ChppGikMetaTask", "a00022.html", null ],
+        [ "ChppGikReachTask", "a00036.html", null ],
+        [ "ChppGikStepBackTask", "a00044.html", null ],
+        [ "ChppGikStepTask", "a00047.html", null ],
+        [ "ChppGikWholeBodyTask", "a00053.html", null ]
+      ] ],
+      [ "ChppGikSolver", "a00040.html", null ],
+      [ "ChppGikSolverBasic", "a00041.html", null ],
+      [ "ChppGikSolverLNE", "a00042.html", null ],
+      [ "ChppGikStandingRobot", "a00043.html", null ],
+      [ "ChppGikStepTarget", "a00046.html", null ],
+      [ "ChppGikSupportPolygon", "a00048.html", null ],
+      [ "ChppGikTools", "a00049.html", null ],
+      [ "ChppGikVectorizableConstraint", "a00051.html", [
+        [ "ChppGikConfigurationConstraint", "a00008.html", null ],
+        [ "ChppGikParallelConstraint", "a00029.html", null ],
+        [ "ChppGikPlaneConstraint", "a00030.html", null ],
+        [ "ChppGikPointingConstraint", "a00031.html", null ],
+        [ "ChppGikPositionConstraint", "a00032.html", null ],
+        [ "ChppGikRotationConstraint", "a00039.html", null ],
+        [ "ChppGikTransformationConstraint", "a00050.html", null ]
+      ] ],
+      [ "ChppRobotMotion", "a00055.html", null ],
+      [ "ChppRobotMotionSample", "a00056.html", null ]
+    ] ],
+    [ "Class Members", "functions.html", null ],
+    [ "File List", "files.html", [
+      [ "com-constraint.hh", "a00057.html", null ],
+      [ "com-motion.hh", "a00058.html", null ],
+      [ "configuration-constraint.hh", "a00059.html", null ],
+      [ "doc.hh", "a00060.html", null ],
+      [ "example.hh", "a00061.html", null ],
+      [ "foot-displace-element.hh", "a00062.html", null ],
+      [ "foot-print-related.hh", "a00063.html", null ],
+      [ "gaze-constraint.hh", "a00064.html", null ],
+      [ "generic-task.hh", "a00065.html", null ],
+      [ "half-sitting-task.hh", "a00066.html", null ],
+      [ "hand-task.hh", "a00067.html", null ],
+      [ "interpolated-element.hh", "a00068.html", null ],
+      [ "joint-state-constraint.hh", "a00069.html", null ],
+      [ "lne-solver.hh", "a00070.html", null ],
+      [ "locomotion-element.hh", "a00071.html", null ],
+      [ "locomotion-plan.hh", "a00072.html", null ],
+      [ "mask-factory.hh", "a00073.html", null ],
+      [ "meta-task.hh", "a00074.html", null ],
+      [ "motion-constraint.hh", "a00075.html", null ],
+      [ "motion-plan-column.hh", "a00076.html", null ],
+      [ "motion-plan-element.hh", "a00077.html", null ],
+      [ "motion-plan-row.hh", "a00078.html", null ],
+      [ "motion-plan.hh", "a00079.html", null ],
+      [ "no-locomotion.hh", "a00080.html", null ],
+      [ "parallel-constraint.hh", "a00081.html", null ],
+      [ "plane-constraint.hh", "a00082.html", null ],
+      [ "pointing-constraint.hh", "a00083.html", null ],
+      [ "position-constraint.hh", "a00084.html", null ],
+      [ "preview-controller.hh", "a00085.html", null ],
+      [ "prioritized-motion.hh", "a00086.html", null ],
+      [ "prioritized-state-constraint.hh", "a00087.html", null ],
+      [ "reach-task.hh", "a00088.html", null ],
+      [ "ready-element.hh", "a00089.html", null ],
+      [ "robot-motion.hh", "a00090.html", null ],
+      [ "robot-task.hh", "a00091.html", null ],
+      [ "rotation-constraint.hh", "a00092.html", null ],
+      [ "shape-2d.hh", "a00093.html", null ],
+      [ "solver-basic.hh", "a00094.html", null ],
+      [ "solver.hh", "a00095.html", null ],
+      [ "standing-robot.hh", "a00096.html", null ],
+      [ "step-back-task.hh", "a00097.html", null ],
+      [ "step-element.hh", "a00098.html", null ],
+      [ "step-task.hh", "a00099.html", null ],
+      [ "tools.hh", "a00100.html", null ],
+      [ "transformation-constraint.hh", "a00101.html", null ],
+      [ "vectorizable-constraint.hh", "a00102.html", null ],
+      [ "walk-element.hh", "a00103.html", null ],
+      [ "whole-body-task.hh", "a00104.html", null ],
+      [ "zmp-shift-element.hh", "a00105.html", null ]
+    ] ],
+    [ "File Members", "globals.html", null ]
+  ] ]
+];
+
+function createIndent(o,domNode,node,level)
+{
+  if (node.parentNode && node.parentNode.parentNode)
+  {
+    createIndent(o,domNode,node.parentNode,level+1);
+  }
+  var imgNode = document.createElement("img");
+  if (level==0 && node.childrenData)
+  {
+    node.plus_img = imgNode;
+    node.expandToggle = document.createElement("a");
+    node.expandToggle.href = "javascript:void(0)";
+    node.expandToggle.onclick = function() 
+    {
+      if (node.expanded) 
+      {
+        $(node.getChildrenUL()).slideUp("fast");
+        if (node.isLast)
+        {
+          node.plus_img.src = node.relpath+"ftv2plastnode.png";
+        }
+        else
+        {
+          node.plus_img.src = node.relpath+"ftv2pnode.png";
+        }
+        node.expanded = false;
+      } 
+      else 
+      {
+        expandNode(o, node, false);
+      }
+    }
+    node.expandToggle.appendChild(imgNode);
+    domNode.appendChild(node.expandToggle);
+  }
+  else
+  {
+    domNode.appendChild(imgNode);
+  }
+  if (level==0)
+  {
+    if (node.isLast)
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2plastnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2lastnode.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+    else
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2pnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2node.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+  }
+  else
+  {
+    if (node.isLast)
+    {
+      imgNode.src = node.relpath+"ftv2blank.png";
+    }
+    else
+    {
+      imgNode.src = node.relpath+"ftv2vertline.png";
+    }
+  }
+  imgNode.border = "0";
+}
+
+function newNode(o, po, text, link, childrenData, lastNode)
+{
+  var node = new Object();
+  node.children = Array();
+  node.childrenData = childrenData;
+  node.depth = po.depth + 1;
+  node.relpath = po.relpath;
+  node.isLast = lastNode;
+
+  node.li = document.createElement("li");
+  po.getChildrenUL().appendChild(node.li);
+  node.parentNode = po;
+
+  node.itemDiv = document.createElement("div");
+  node.itemDiv.className = "item";
+
+  node.labelSpan = document.createElement("span");
+  node.labelSpan.className = "label";
+
+  createIndent(o,node.itemDiv,node,0);
+  node.itemDiv.appendChild(node.labelSpan);
+  node.li.appendChild(node.itemDiv);
+
+  var a = document.createElement("a");
+  node.labelSpan.appendChild(a);
+  node.label = document.createTextNode(text);
+  a.appendChild(node.label);
+  if (link) 
+  {
+    a.href = node.relpath+link;
+  } 
+  else 
+  {
+    if (childrenData != null) 
+    {
+      a.className = "nolink";
+      a.href = "javascript:void(0)";
+      a.onclick = node.expandToggle.onclick;
+      node.expanded = false;
+    }
+  }
+
+  node.childrenUL = null;
+  node.getChildrenUL = function() 
+  {
+    if (!node.childrenUL) 
+    {
+      node.childrenUL = document.createElement("ul");
+      node.childrenUL.className = "children_ul";
+      node.childrenUL.style.display = "none";
+      node.li.appendChild(node.childrenUL);
+    }
+    return node.childrenUL;
+  };
+
+  return node;
+}
+
+function showRoot()
+{
+  var headerHeight = $("#top").height();
+  var footerHeight = $("#nav-path").height();
+  var windowHeight = $(window).height() - headerHeight - footerHeight;
+  navtree.scrollTo('#selected',0,{offset:-windowHeight/2});
+}
+
+function expandNode(o, node, imm)
+{
+  if (node.childrenData && !node.expanded) 
+  {
+    if (!node.childrenVisited) 
+    {
+      getNode(o, node);
+    }
+    if (imm)
+    {
+      $(node.getChildrenUL()).show();
+    } 
+    else 
+    {
+      $(node.getChildrenUL()).slideDown("fast",showRoot);
+    }
+    if (node.isLast)
+    {
+      node.plus_img.src = node.relpath+"ftv2mlastnode.png";
+    }
+    else
+    {
+      node.plus_img.src = node.relpath+"ftv2mnode.png";
+    }
+    node.expanded = true;
+  }
+}
+
+function getNode(o, po)
+{
+  po.childrenVisited = true;
+  var l = po.childrenData.length-1;
+  for (var i in po.childrenData) 
+  {
+    var nodeData = po.childrenData[i];
+    po.children[i] = newNode(o, po, nodeData[0], nodeData[1], nodeData[2],
+        i==l);
+  }
+}
+
+function findNavTreePage(url, data)
+{
+  var nodes = data;
+  var result = null;
+  for (var i in nodes) 
+  {
+    var d = nodes[i];
+    if (d[1] == url) 
+    {
+      return new Array(i);
+    }
+    else if (d[2] != null) // array of children
+    {
+      result = findNavTreePage(url, d[2]);
+      if (result != null) 
+      {
+        return (new Array(i).concat(result));
+      }
+    }
+  }
+  return null;
+}
+
+function initNavTree(toroot,relpath)
+{
+  var o = new Object();
+  o.toroot = toroot;
+  o.node = new Object();
+  o.node.li = document.getElementById("nav-tree-contents");
+  o.node.childrenData = NAVTREE;
+  o.node.children = new Array();
+  o.node.childrenUL = document.createElement("ul");
+  o.node.getChildrenUL = function() { return o.node.childrenUL; };
+  o.node.li.appendChild(o.node.childrenUL);
+  o.node.depth = 0;
+  o.node.relpath = relpath;
+
+  getNode(o, o.node);
+
+  o.breadcrumbs = findNavTreePage(toroot, NAVTREE);
+  if (o.breadcrumbs == null)
+  {
+    o.breadcrumbs = findNavTreePage("index.html",NAVTREE);
+  }
+  if (o.breadcrumbs != null && o.breadcrumbs.length>0)
+  {
+    var p = o.node;
+    for (var i in o.breadcrumbs) 
+    {
+      var j = o.breadcrumbs[i];
+      p = p.children[j];
+      expandNode(o,p,true);
+    }
+    p.itemDiv.className = p.itemDiv.className + " selected";
+    p.itemDiv.id = "selected";
+    $(window).load(showRoot);
+  }
+}
+
