9f62919149e5958ff1b3a5bb8d114b02bf826dea
Sébastien Dalibard <sebastien.dalibard@gmail.com>
2011-11-19 19:02:25 +0800

    Add new constraint to fix the com x,y position relatively to a given joint

---
+++ include/hpp/gik/constraint/relative-com-constraint.hh
@@ -0,0 +1,68 @@
+#ifndef HPP_GIK_RELATIVE_COM_CONSTRAINT_H
+#define HPP_GIK_RELATIVE_COM_CONSTRAINT_H
+
+#include <hpp/gik/constraint/com-constraint.hh>
+
+/**
+   \brief Specify a relative position constraint on the center of mass.
+   \ingroup constraints
+*/
+
+class ChppGikRelativeComConstraint: public ChppGikComConstraint
+{
+public:
+  /**
+     \name Definition of the constraint
+     @{
+  */
+
+  /**
+     \brief Constructor
+     @param inRobot Robot
+     @param inJoint Joint in which the CoM position is computed
+     @param inX CoM target position
+     @param inY CoM target position
+  */
+  ChppGikRelativeComConstraint(CjrlDynamicRobot& inRobot, CjrlJoint* inJoint, double inX, double inY);
+  
+  /**
+     \brief Copy the object
+  */
+  CjrlGikStateConstraint* clone() const;
+
+     /**
+    \brief Compute the value of the constraint.
+     */
+    virtual void computeValue();
+
+   /**
+    \brief Get the constraint Jacobian wrt internal configuration variables.
+   */
+    virtual void computeJacobian();
+
+   /**
+    \brief Destructor
+     */
+  virtual ~ChppGikRelativeComConstraint();
+
+private:
+  CjrlDynamicRobot* robot_;
+
+  CjrlJoint *rootJoint_;
+
+  matrixNxP comJacobian_;
+  matrixNxP posJacobian_;
+  matrixNxP jacobian_;
+
+  matrixNxP jointRot_;
+
+  vectorN comPos_;
+  vectorN jointTr_;
+  vectorN worldTarget_;
+  vectorN value_;
+
+  vectorN localTarget_;
+
+};
+
+#endif
---
+++ src/constraint/relative-com-constraint.cc
@@ -0,0 +1,72 @@
+#include "hpp/gik/constraint/relative-com-constraint.hh"
+
+
+
+ChppGikRelativeComConstraint::ChppGikRelativeComConstraint(CjrlDynamicRobot& inRobot, 
+							   CjrlJoint* inJoint, 
+							   double inX, 
+							   double inY):
+  ChppGikComConstraint(inRobot,inX,inY)
+{
+  robot_ = &inRobot;
+  rootJoint_ = inJoint;
+  jacobian_.resize(2,robot_->numberDof(),false);
+  comJacobian_.resize(3,robot_->numberDof(),false);
+  posJacobian_.resize(3,robot_->numberDof(),false);
+
+  jointRot_.resize(3,3,false);
+  jointTr_.resize(3,false);
+
+  comPos_.resize(3,false);
+  worldTarget_.resize(3,false);
+  value_.resize(2),false;
+
+  localTarget_.resize(3,false);
+  localTarget_(0) = inX;
+  localTarget_(1) = inY;
+  localTarget_(2) = 0;
+
+}
+
+ChppGikRelativeComConstraint::~ChppGikRelativeComConstraint()
+{
+}
+
+CjrlGikStateConstraint* 
+ChppGikRelativeComConstraint::clone() const
+{
+  CjrlGikStateConstraint* ret = new ChppGikRelativeComConstraint(*this);
+  return ret;
+}
+
+void
+ChppGikRelativeComConstraint::computeValue()
+{
+  ChppGikTools::Vector3toUblas( robot_->positionCenterOfMass(), comPos_);
+  ChppGikTools::HtoRT(joint->currentTransformation(),jointRot_,jointTr_);
+  worldTarget_ = prod(jointRot_,localTarget_) + jointTr_;
+
+  value_ = subrange(comPos_ - worldTarget_,0,2);
+}
+
+void
+ChppGikRelativeComConstraint::computeJacobian()
+{
+  robot().getJacobianCenterOfMass(*robot().rootJoint(),comJacobian_,0,false);
+  robot().getPositionJacobian(*robot().rootJoint(),*rootJoint_,localTarget_,posJacobian_,0,false);
+
+  row(jacobian_,0) = row(comJacobian_,0) - row(posJacobian_,0);
+  row(jacobian_,1) = row(comJacobian_,1) - row(posJacobian_,1);
+}
+
+const vectorN& 
+ChppGikRelativeComConstraint::value()
+{
+    return value_;
+}
+
+const matrixNxP& 
+ChppGikRelativeComConstraint::jacobian()
+{
+    return jacobian_;
+}
